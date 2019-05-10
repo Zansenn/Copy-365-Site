1894173ddb38358da6a95c842bbb2979601dc174
Naz <nazz.sen@gmail.com>
2019-05-10 16:36:08 +0100

    Managed to import assignmstr module to copyfrommstr module

--- copyfrommstr.py
+++ copyfrommstr.py
@@ -3,12 +3,15 @@
 import json
 import shutil
 correct_input = ("y","n")
+mstr_dir = 0
 
 usercopy = input ("What would you like to do? to copy from master directory to new director type cdir\n")
 if usercopy == "cdir":
     with open('mstr_dir.txt') as json_file:
         mstr_dir = json.load(json_file)
         print(mstr_dir)
+else:
+    exit()
 
 print("above is the directory you will copy to: \n")
 #testing that the user has inputted the correct information
@@ -16,7 +19,8 @@ while correct_input == ("y","n"):
     if usercopy == ("y"):
         break
     if usercopy == "n":
-        #run the assignmstr.py. You need to add this in so for now "n" wont work
+        import assignmstr
+
         usercopy =input("please write y/n\n")
         continue
     if usercopy is not correct_input:
